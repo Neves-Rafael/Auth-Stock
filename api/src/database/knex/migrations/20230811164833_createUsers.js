661be67d5d0b93f325241bbe468fe00404e356dd
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-25 17:22:05 -0300

    :sparkles: Feat new table in migrations

--- api/src/database/knex/migrations/20230811164833_createUsers.js
+++ api/src/database/knex/migrations/20230811164833_createUsers.js
@@ -1,11 +1,20 @@
-exports.up = knex => knex.schema.createTable("users", table => {
-  table.increments("id");
-  table.text("name").notNullable();
-  table.text("email").notNullable();
-  table.text("password").notNullable();
+exports.up = (knex) =>
+  knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.text("name").notNullable();
+    table.text("email").notNullable();
+    table.text("password").notNullable();
 
-  table.timestamp("created_at").default(knex.fn.now());
-  table.timestamp("updated_at").default(knex.fn.now());
-});
+    table
+      .enum("role", ["admin", "customer"], {
+        useNative: true,
+        enumName: "roles",
+      })
+      .notNullable()
+      .default("customer");
 
-exports.down = knex => knex.schema.dropTable("users");
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("users");
