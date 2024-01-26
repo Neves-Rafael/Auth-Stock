c51146b9d28a2f52d3a855568b7f261c3b38ad39
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-26 16:38:47 -0300

    :lipsstick: Style dynamic card in home page

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,19 +1,40 @@
-import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
+import { FiTruck, FiTag, FiShoppingCart } from "react-icons/fi";
 
 import { Container } from "./styles";
-import { Feature } from '../../components/Feature';
-import { Header } from '../../components/Header';
+import { Feature } from "../../components/Feature";
+import { Header } from "../../components/Header";
+
+import { useAuth } from "../../hooks/auth";
+import { USER_ROLES } from "../../utils/roles";
 
 export function Home() {
+  const { user } = useAuth();
+
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+        {user.role === USER_ROLES.ADMIN && (
+          <>
+            <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
+            <Feature
+              title="Relatório de vendas"
+              icon={FiShoppingCart}
+              to="/sales-report"
+            />
+          </>
+        )}
+
+        {user.role === USER_ROLES.SALE && (
+          <Feature
+            title="Relatório de vendas"
+            icon={FiShoppingCart}
+            to="/sales-report"
+          />
+        )}
       </main>
     </Container>
-  )
+  );
 }
