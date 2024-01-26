83e79231006cd4a9cd47541d2a14b0748b6a2ffa
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-26 17:23:12 -0300

    :recycle: Refactor changed to sending the token via cookie

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -27,8 +27,15 @@ class SessionsController {
       expiresIn,
     });
 
+    response.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      maxAge: 15 * 60 * 1000,
+    });
+
     delete user.password;
-    response.status(201).json({ token, user });
+    response.status(201).json({ user });
   }
 }
 
