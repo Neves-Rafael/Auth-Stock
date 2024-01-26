e129fb8766876f5f3042d4c0da140851bdfbb562
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-26 16:00:32 -0300

    :sparkles: Feat separete routes

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 import { useAuth } from "../hooks/auth";
 
-import { AppRoutes } from './app.routes';
-import { AuthRoutes } from './auth.routes';
+import { AdminRoutes } from "./admin.routes";
+import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   const { user } = useAuth();
 
   return (
-    <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
-    </BrowserRouter>
+    <BrowserRouter>{user ? <AdminRoutes /> : <AuthRoutes />}</BrowserRouter>
   );
 }
