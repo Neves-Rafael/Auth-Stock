57ef247039f87981431715146f31fcea31edb5e0
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-26 16:27:12 -0300

    :lipsstick: Style dynamic role in header component

--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -1,28 +1,30 @@
-import { FiUser, FiLogOut } from 'react-icons/fi';
+import { FiUser, FiLogOut } from "react-icons/fi";
 
-import { useAuth } from '../../hooks/auth';
+import { useAuth } from "../../hooks/auth";
 
-import { Container, User } from './styles';
+import { Container, User } from "./styles";
 
 export function Header() {
-    const { signOut, user } = useAuth();
+  const { signOut, user } = useAuth();
 
-    return (
-        <Container>
-            <h1>Menu</h1>
+  return (
+    <Container>
+      <h1>Menu</h1>
 
-            <aside>
-                <User>
-                    <span>Olá, <strong>{user.name}</strong></span>
-                    <small>
-                        <FiUser /> Perfil do usuário
-                    </small>
-                </User>
-            </aside>
+      <aside>
+        <User>
+          <span>
+            Olá, <strong>{user.name}</strong>
+          </span>
+          <small>
+            <FiUser /> Perfil de: {user.role}
+          </small>
+        </User>
+      </aside>
 
-            <button type="button" onClick={signOut}>
-                <FiLogOut size={24} />
-            </button>
-        </Container>
-    );
-};
+      <button type="button" onClick={signOut}>
+        <FiLogOut size={24} />
+      </button>
+    </Container>
+  );
+}
