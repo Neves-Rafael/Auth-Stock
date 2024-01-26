9bd7cd0a22840c1e049cf8a738c718ec18749b2d
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-26 19:59:07 -0300

    :sparkles: Feat using cookie to authorize

--- web/src/pages/SalesReport/index.jsx
+++ web/src/pages/SalesReport/index.jsx
@@ -1,10 +1,17 @@
 import { Container } from "./styles";
+import { useEffect } from "react";
+import { api } from "../../services/api";
 
 export function SalesReport() {
+  useEffect(() => {
+    api
+      .get("/sales", { withCredentials: true })
+      .then((response) => console.log(response.data));
+  });
+
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
     </Container>
-
-  )
+  );
 }
