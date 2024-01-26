5cd4748d70f4f78091d8071862149f4c71664a71
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-26 16:14:59 -0300

    :sparkles: Feat assign routes

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,14 +1,30 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from "../utils/roles";
 
 import { AdminRoutes } from "./admin.routes";
 import { AuthRoutes } from "./auth.routes";
+import { CustomerRoutes } from "./customer.routes";
+import { SaleRoutes } from "./sale.routes";
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      case USER_ROLE.SALE:
+        return <SaleRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }
+
   return (
-    <BrowserRouter>{user ? <AdminRoutes /> : <AuthRoutes />}</BrowserRouter>
+    <BrowserRouter>{user ? <AccessRoute /> : <AuthRoutes />}</BrowserRouter>
   );
 }
---
+++ web/src/utils/roles.js
@@ -0,0 +1,5 @@
+export const USER_ROLES = {
+  ADMIN: "admin",
+  CUSTOMER: "customer",
+  SALE: "sale",
+};
